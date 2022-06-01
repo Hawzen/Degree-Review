8bca5731104c3644d93d6afdcb28c0b0a6b084fc
Hawzen <hawzen@users.noreply.github.com>
2022-06-01 10:40:39 +0300

    Add fetch_commits.py

---
+++ fetch_commits.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+from github import Github
+from pandas import to_datetime
+
+with open(".env") as file:
+    env = {e.split("=")[0]: e.split("=")[1] for e in file.read().split("\n") if e}
+
+db = sqlite3.connect("db.sqlite3")
+g = Github(env["github_access_token"])
+
+cursor = db.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS commits(
+        repo TEXT not NULL,
+        date DATE not NULL,
+        rawData TEXT not NULL
+    )
+""")
+db.commit()
+
+for repo in g.get_user().get_repos():
+    cursor = db.cursor()
+    records = [(repo.name, str(to_datetime(commit.raw_data["commit"]["committer"]["date"])), str(commit.raw_data)) for commit in repo.get_commits()]
+    cursor.executemany("""
+            INSERT INTO commits(repo, date, rawData)
+            values(?, ?, ?)
+        """, (records))
+    db.commit()
